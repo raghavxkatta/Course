dc3389927a890e5c7cc9fd7594f56ec563ca2c8e
Raghav Katta <Raghavkatta2005@gmail.com>
2024-08-27 23:33:55 +0530

    Error 404 not found

--- middleware-Intro/index.js
+++ middleware-Intro/index.js
@@ -2,23 +2,25 @@ const express= require('express');
 const app= express();
 const morgan= require('morgan')
 /* Middleware for logging HTTP requests */
-// app.use(morgan('common'))
+app.use(morgan('common'))
 app.use((req,res,next)=>{
     req.requestTime=Date.now()
     console.log(req.method.toUpperCase(),req.path)
 next()  
 })
 
-app.get('/',()=>{
+app.get('/',(req,res)=>{
     console.log(`Request Date:${req.requestTime}`)
     res.send('HOME PAGE')
     
 })
-app.get('/dogs',()=>{
-
+app.get('/dogs',(req,res)=>{
     console.log(`Request Date:${req.requestTime}`)
-
     res.send('WOOF WOOF!')
+})
+/* when no route matches then this middleware comes to use */
+app.use((req,res)=>{
+    res.send('Not found');
 })
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
