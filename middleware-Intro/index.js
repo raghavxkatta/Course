38cfbb0cec786dd5258c863606a51eeaa987384a
Raghav Katta <Raghavkatta2005@gmail.com>
2024-08-27 23:57:47 +0530

    protecting specific routes-1

--- middleware-Intro/index.js
+++ middleware-Intro/index.js
@@ -3,9 +3,14 @@ const app= express();
 const morgan= require('morgan')
 /* Middleware for logging HTTP requests */
 app.use(morgan('common'))
-app.use(req,res,next)=>{
+app.use(req,res,next=>{
+    if(req.path==='./secret'){/* so now you need password to open the secret route, the rest you are free to open without password */
     const{password}=req.query
-}
+if(password==='chickenNugget'){/* the correct password is chickenNugget */
+    next();
+}}
+res.send("Sorry but you need a password")/* without the correct password in the query string, this will be the only thing being displayed and you won't be able to move ahead */
+})
 app.use((req,res,next)=>{
     req.requestTime=Date.now()
     console.log(req.method.toUpperCase(),req.path)
@@ -20,6 +25,10 @@ app.get('/',(req,res)=>{
 app.get('/dogs',(req,res)=>{
     console.log(`Request Date:${req.requestTime}`)
     res.send('WOOF WOOF!')
+})
+app.get('/secret',(req,res)=>{/* this route will only run when secret is the route and you have entered the correct password as query string */
+    res.send("I have many secrets")
+    
 })
 /* when no route matches then this middleware comes to use */
 app.use((req,res)=>{
