6d35e19e1dbdcf19ee0b06eb4408c4a077099649
Raghav Katta <Raghavkatta2005@gmail.com>
2024-11-01 18:17:35 +0530

    fixed error handling problem

--- HandlingAsyncErrors/AppError.js
+++ HandlingAsyncErrors/AppError.js
@@ -1,6 +1,6 @@
 class AppError extends Error {
 constructor(message,status){
-    super()/* this is used so that we can access parent class methods and call overridenn methods of the parent class */
+    super(message)/* this is used so that we can access parent class methods and call overridenn methods of the parent class */
 this.message=message
 this.status=status
 }
