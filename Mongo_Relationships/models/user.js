af1732a738fa689ecd69f1d82734be3c4ddcb87f
Raghav Katta <Raghavkatta2005@gmail.com>
2024-12-01 19:46:23 +0530

    addAddresses

--- Mongo_Relationships/models/user.js
+++ Mongo_Relationships/models/user.js
@@ -1,40 +1,51 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/mongo_relationship')
-.then(()=>{
-console.log("connection open");
-})
-.catch((err)=>{
-    console.log("Connection error",err);
-})
+const mongoose = require("mongoose");
+mongoose
+    .connect("mongodb://127.0.0.1:27017/mongo_relationship")
+    .then(() => {
+        console.log("connection open");
+    })
+    .catch((err) => {
+        console.log("Connection error", err);
+    });
 
-const userSchema= new mongoose.Schema({
-    first:String,
-    last:String,
-    addresses:[
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses: [
         {
-street: String,
-city:String,
-State:String,
-country:String
-        }
-    ]
-})
-const User = mongoose.model('User',userSchema)
+            street: String,
+            city: String,
+            State: String,
+            country: String,
+        },
+    ],
+});
+const User = mongoose.model("User", userSchema);
 
-const makeUser= async()=>{
-const u= new User({
-first:'Harry',
-last:'Singh'
-})
-u.addresses.push({
-    street:'123 Sesame Street',
-    city:'New York',
-    State:'NY',
-    country:'United States of America'
-})
-const res=await u.save()
-console.log(res)
-}
+const makeUser = async () => {
+    const u = new User({
+        first: "Harry",
+        last: "Singh",
+    });
+    u.addresses.push({
+        street: "123 Sesame Street",
+        city: "New York",
+        State: "NY",
+        country: "United States of America",
+    });
+    const res = await u.save();
+    console.log(res);
+};
+
+const addAddress = async (id) => {
+    const user = await User.findById(id);
+    user.addresses.push({
+        street: "99 3rd Street",
+        city: "New York",
+        State: "NY",
+        country: "United States of America",
+    });
+    const res = await user.save();
+    console.log(res);
+};
 makeUser();
-
-
