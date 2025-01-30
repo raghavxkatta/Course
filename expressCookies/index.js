122eef225c0f937b4231da3e272226a0a469c430
Raghav Katta <Raghavkatta2005@gmail.com>
2025-01-30 09:42:54 +0530

    Requesting Cookies from the user

--- expressCookies/index.js
+++ expressCookies/index.js
@@ -11,8 +11,11 @@ app.get('/greet',(req,res)=>{
 // Sending a Cookie
 app.get('/setname',(req,res)=>{
     /* So basically what it does this sends a cookie to user wheneever the user goes on the link setname and once it goes even one time them this value stays there throughout, it is stored */
+    /* If we now open any page this will be shown as the name, I mean whatever you wrote in the cookies as your name and if no name then it's Raghav */
+
+    const{name="Raghav"}=req.cookies
     res.cookie('name','Henry')
-    res.send("Ok sent you a cookie")
+    res.send(`Hey ${name} sent you a cookie`)
 })
 
 app.listen(3000,()=>{
