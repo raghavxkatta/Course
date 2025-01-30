13b70afd52d0a2af09f48dc8b40ef00d8908d340
Raghav Katta <Raghavkatta2005@gmail.com>
2025-01-30 09:59:13 +0530

    Signing Cookies

--- expressCookies/index.js
+++ expressCookies/index.js
@@ -2,7 +2,9 @@
 
 const express= require('express')
 const app = express()
-
+const cookieParser=require ('cookie-parser')
+/* Cookie Parser will now use this to check, it will basically compare the cookie that user sent with what the client stored */
+app.use(cookieParser('thisismysecret'))
 app.get('/greet',(req,res)=>{
     res.send("Hey There")
 })
@@ -16,6 +18,22 @@ app.get('/setname',(req,res)=>{
     const{name="Raghav"}=req.cookies
     res.cookie('name','Henry')
     res.send(`Hey ${name} sent you a cookie`)
+})
+
+
+
+/* Signed Cookies */
+/* It is a way of verifying something,so basically it's just check if the cookie is in the exact same situation as it was when it was just sent, so for eg if we take the example of the below cookie there we took grape a fruit as a cookie, now if someone tampers with that cookie and changes it to something else, it would'n't display it as while comparing it finds that this is not the same as original and that is signed cookies for you */
+    
+app.get('/verify',(req,res)=>{
+    res.cookies('fruit','grape',{signed:true})/* Stored the fruit as a grape in the form of cookie in the browser and signed it as well */
+    res.send("Okay sent your fruit cookie")
+})
+
+app.get('/verifyfruit',(req,res)=>{
+    console.log(req.Cookies)
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
 })
 
 app.listen(3000,()=>{
